0c5eb2a0d853c27cd1afd3143459ef3067692661
airtonsena10 <airtonsena41@gmail.com>
2022-06-12 17:32:15 -0300

    feat: sparkles created component Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { Post } from './Post';
+import { Header } from "./components/Header";
 
-
-
+import './global.css';
 export function App() {
   return (
-   
-   <Post />
+   <Header />
+
   )
 }
 
