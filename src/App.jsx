3daa14cc3d28857efbfe9fde5cc09630eb5ee690
airtonsena10 <airtonsena41@gmail.com>
2022-06-17 15:54:25 -0300

    :sparkles: feat: created component Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from './components/Post';
 
-import './global.css';
+import "./global.css";
+import styles from './App.module.css';
 export function App() {
   return (
-   <Header />
+    <div>
+      <Header />
 
-  )
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Fulano"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Eius consectetur, nobis repellat fugiat porro laborum ad reiciendis? 
+            Voluptatum ipsum cum ipsam perferendis voluptatem labore perspiciatis 
+            aliquam dolorum velit, illo exercitationem!"
+          />
+          <Post
+            author="Fulano Two"
+            content="Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Eius consectetur, nobis repellat 
+            fugiat porro laborum ad reiciendis? Voluptatum ipsum cum ipsam 
+            perferendis voluptatem labore perspiciatis aliquam dolorum velit, 
+            illo exercitationem!"
+          />
+        </main>
+      </div>
+    </div>
+  );
 }
-
-
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+      />
+
+      <div className={styles.profile}>
+        <img 
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/65772731?v=4" 
+        />
+
+        <strong>Airton Sena</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+} 
