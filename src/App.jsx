6f65dd2080b776e45901c28348f2615c4cf9c52f
airtonsena10 <airtonsena41@gmail.com>
2022-06-17 16:20:00 -0300

    :sparkles:  feat: created component Comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import { Post } from './components/Post';
+import { Post } from "./components/Post";
 
 import "./global.css";
-import styles from './App.module.css';
+import styles from "./App.module.css";
 export function App() {
   return (
     <div>
@@ -12,21 +12,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Fulano"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Eius consectetur, nobis repellat fugiat porro laborum ad reiciendis? 
-            Voluptatum ipsum cum ipsam perferendis voluptatem labore perspiciatis 
-            aliquam dolorum velit, illo exercitationem!"
-          />
-          <Post
-            author="Fulano Two"
-            content="Lorem ipsum dolor sit amet consectetur 
-            adipisicing elit. Eius consectetur, nobis repellat 
-            fugiat porro laborum ad reiciendis? Voluptatum ipsum cum ipsam 
-            perferendis voluptatem labore perspiciatis aliquam dolorum velit, 
-            illo exercitationem!"
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        src="https://avatars.githubusercontent.com/u/65772731?v=4"
+        alt="" 
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Airton sena <span>(Você)</span></strong>
+              <time title="04 de Junho de 2022 às 22:00" dateTime="2022-06-04T22:00:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp/>
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+} 
